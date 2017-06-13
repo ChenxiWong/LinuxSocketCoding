249d111d52251334ae4ee73804d511fbcf93be21
wangchenxi <chinawangchenxi@gmail.com>
2017-06-13 11:55:39 +0800

    some prepared.

---
+++ Pro*C/git_prepared.sh
@@ -0,0 +1,10 @@
+# Last Update:2017-06-13 11:55:00
+#########################################################################
+# File Name: git_prepared.sh
+# Author: wangchenxi
+# mail: chinawangchenxi@gmail.com
+# Created Time: 2017年06月13日 星期二 11时55分00秒
+# brief:
+#########################################################################
+#!/bin/bash
+rm -rf demo*.c *.lis
